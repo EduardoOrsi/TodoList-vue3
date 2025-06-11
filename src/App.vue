ee291cce7962cb560b61f89d210e0c8d4a695c80
Eduardo Orsi <eduardo.orsi@phddobrasil.com>
2025-06-11 11:17:41 -0300

    chore: ajusta estilização

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,26 @@ const filters: TaskFilter[] = ['all', 'todo', 'done']
 
 <template>
   <main>
-    <h3>Task App</h3>
     <TaskForm />
     <h3 v-if="!store.tasks.length">Adicione uma tarefa para começar</h3>
-    <h3 v-else>{{ store.totalDone }}/{{ store.tasks.length }} tarefas concluídas.</h3>
-    <ButtonFilter v-if="store.tasks.length" v-for="filter in filters" :key="filter" :filter
-      :currentFilter="store.filter" @set-filter="store.setFilter" />
+    <h3 v-else>{{ store.totalDone }} / {{ store.tasks.length }} tarefas concluídas.</h3>
+    <div v-if="store.tasks.length" class="button-container">
+      <ButtonFilter class="button-container" v-if="store.tasks.length" v-for="filter in filters" :key="filter" :filter
+        :currentFilter="store.filter" @set-filter="store.setFilter" />
+    </div>
     <TaskList :tasks="store.filteredTasks" />
   </main>
 </template>
+
+<style>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: end;
+  gap: 0.5rem
+}
+</style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -5,17 +5,40 @@ const store = useTaskStore()
 </script>
 
 <template>
-    <div v-for="task in store.filteredTasks" :key="task.id">
-        <label>
-            <input type="checkbox" :checked="task.done" @change="store.toggleDone(task.id)" />
-            <span :class="{ done: task.done }">{{ task.title }}</span>
-        </label>
-        <button @click="store.remove(task.id)">Remover</button>
-    </div>
+    <TransitionGroup class="task-list" name="task-list" tag="div">
+        <article v-for="task in store.filteredTasks" :key="task.id" class="task">
+            <label>
+                <input type="checkbox" :checked="task.done" @change="store.toggleDone(task.id)" />
+                <span :class="{ done: task.done }">{{ task.title }}</span>
+            </label>
+            <button class="outline" @click="store.remove(task.id)">Remover</button>
+        </article>
+    </TransitionGroup>
 </template>
 
 <style scoped>
+.task-list {
+    margin-top: 1rem;
+}
+
+.task {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
 .done {
     text-decoration: line-through;
+}
+
+.task-list-enter-active,
+.task-list-leave-active {
+    transition: all 0.5s ease;
+}
+
+.task-list-enter-from,
+.task-list-leave-to {
+    opacity: 0;
+    transform: translateX(300px);
 }
 </style>
